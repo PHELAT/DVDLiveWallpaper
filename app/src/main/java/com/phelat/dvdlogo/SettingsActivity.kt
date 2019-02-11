08f6b17cadfc1cf7d3bdd61a687c6a54e499c69c
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2019-02-11 14:08:53 +0330

    Set content view for SettingsActivity

--- app/src/main/java/com/phelat/dvdlogo/SettingsActivity.kt
+++ app/src/main/java/com/phelat/dvdlogo/SettingsActivity.kt
@@ -1,5 +1,13 @@
 package com.phelat.dvdlogo
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class SettingsActivity : AppCompatActivity()
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+
+}
