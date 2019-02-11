f6f72d9a5ce65244fda3bf8a260b59504825b63a
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2019-02-11 03:32:28 +0330

    Draw DVD logo on the center of canvas

--- app/src/main/java/com/phelat/dvdlogo/DvdWallpaperService.kt
+++ app/src/main/java/com/phelat/dvdlogo/DvdWallpaperService.kt
@@ -1,8 +1,12 @@
 package com.phelat.dvdlogo
 
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.service.wallpaper.WallpaperService
 import android.view.SurfaceHolder
 
@@ -18,6 +22,20 @@ class DvdWallpaperService : WallpaperService() {
     inner class DvdLogoEngine : Engine() {
 
         private var canvas: Canvas? = null
+
+        private val bitmap by lazy(LazyThreadSafetyMode.NONE) {
+            BitmapFactory.decodeResource(context.resources, R.drawable.dvd)
+        }
+
+        private val paint by lazy(LazyThreadSafetyMode.NONE) {
+            Paint().apply {
+                isAntiAlias = true
+                colorFilter = PorterDuffColorFilter(
+                    Color.parseColor("#FFFFFF"),
+                    PorterDuff.Mode.SRC_IN
+                )
+            }
+        }
 
         override fun onSurfaceCreated(holder: SurfaceHolder?) {
             super.onSurfaceCreated(holder)
@@ -41,6 +59,12 @@ class DvdWallpaperService : WallpaperService() {
         private fun draw() {
             canvas?.apply {
                 drawColor(Color.parseColor("#000000"))
+                drawBitmap(
+                    bitmap,
+                    (width - bitmap.width).toFloat() / 2,
+                    (height - bitmap.height).toFloat() / 2,
+                    paint
+                )
             }
         }
 
